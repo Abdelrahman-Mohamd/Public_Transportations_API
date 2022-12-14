104c4c263dd30b101aa28a2e85ee54ee3f05685c
Omarzakaria2085 <omarzakaria2085@gmail.com>
2022-12-14 13:58:56 +0200

    Creating orderByCost function

--- src/models/user.js
+++ src/models/user.js
@@ -10,16 +10,71 @@ const driver = neo4j.driver(url, neo4j.auth.basic(db_username, db_password));
 const session = driver.session({ database });
 
 //function to return all database nodes
-const findAll = async () =>{
+const findAll = async () => {
     const result = await session.run(`MATCH (n) RETURN n`)
     return {allNodes:result.records.map(i=>i.get('n').properties)}
 }
 
 //Omar: write a function to return cost
 
+const orderByCost = async (sourceNode,destinationNode) =>{
+
+    let result = await session.run(`
+    MATCH (n1:LOCATION {name: '${sourceNode}'} ),
+     (n2:LOCATION {name: '${destinationNode}'}),
+     p =(n1)-[:LINE*]->(n2)
+     With count(*) as numberOfAvailablePaths
+     MATCH (n1:LOCATION {name: '${sourceNode}'} ),
+     (n2:LOCATION {name: '${destinationNode}'}),
+     path =(n1)-[:LINE*]->(n2)
+     return path,
+     numberOfAvailablePaths,
+     reduce(s=0, i in relationships(path) | s+i.cost ) as totalCost,
+     reduce(s=0, i in relationships(path) | s+i.distance ) as totalDistance,
+     size(nodes(path)) as numberOfStops
+     order By totalCost,totalDistance`)
+
+    let paths = [];
+    let path =[];
+    let records = result.records;
+    let recordNo ,segmentNo ;
+    let recordsLength = `${records.length}`;
+
+    for (recordNo = 0; recordNo<recordsLength; recordNo ++ ){   
+        let segments = result.records[recordNo]._fields[0].segments;
+        let segmentsLength = `${segments.length}`;
+        let totalCost =0 ;let totalDistance = 0;
+        for (segmentNo = 0; segmentNo < segmentsLength; segmentNo++) {
+            path.push({
+                name: segments[segmentNo].start.properties.name, 
+                latitude: segments[segmentNo].start.properties.latitude,
+                longitude: segments[segmentNo].start.properties.longtude,                
+                cost: segments[segmentNo].relationship.properties.cost.low,
+                distance:segments[segmentNo].relationship.properties.distance,
+                transportationType:segments[segmentNo].relationship.properties.type,
+                lineNumber:segments[segmentNo].relationship.properties.name
+            });
+            totalCost+=segments[segmentNo].relationship.properties.cost.low;
+            totalDistance+=segments[segmentNo].relationship.properties.distance;
+            if (segmentNo == segmentsLength - 1) {
+                path.push({
+                    name: segments[segmentNo].end.properties.name, 
+                    latitude: segments[segmentNo].end.properties.latitude,
+                    longitude: segments[segmentNo].end.properties.longtude,                
+                    totalCost:totalCost,
+                    totalDistance:totalDistance
+                });
+            }
+    }
+    paths.push(path);
+    path = [];
+}
+    
+    return paths;
+}
 
 module.exports = {
-    findAll,
+    findAll,orderByCost
     //Omar: write your function name here
 }
    
